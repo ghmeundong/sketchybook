d995447367ce51f88dd1c3c3508ffa335a5a2bae
김동은 <comflex3719@gmail.com>
2026-07-05 10:04:11 +0900

    feat: mobile landscape

--- src/game/main.js
+++ src/game/main.js
@@ -2,6 +2,7 @@ import rough from "roughjs";
 import "../style.css";
 import "../styles/game.css";
 import paperTexture from "../img/paper-texture.webp";
+import { initializeOrientationPrompt } from "./orientationPrompt.js";
 import { createCoordinateSystem } from "./coordinates.js";
 import { loadStage } from "./stageLoader.js";
 import {
@@ -147,6 +148,7 @@ if (backHomeButton) {
 }
 
 refreshStageSelectionButtons();
+initializeOrientationPrompt();
 
 function resetStageState() {
   if (animationFrameId) {
